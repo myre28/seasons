cb95f75c737191da26d6cb5d43e63f49c911a750
Bwamutala Miantezila <bwamutalamiantezila@MacBook-Pro.attlocal.net>
2020-08-18 16:23:51 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+class App extends React.Component {
+    /*  constructor(props){
+         super(props);
+         //THIS IS GTHE ONLY TIME we do direct assignment
+         this.state={lat : null, errorMessage: '' };     
+    } */
+
+
+
+    state={lat : null, errorMessage: '' };
+    componentDidMount(){
+
+      window.navigator.geolocation.getCurrentPosition(
+        position=> this.setState({lat: position.coords.latitude}),
+         err => this.setState ({errorMessage : err.message})        
+      );
+
+     }
+
+
+    //React says we have to define render!!
+    render(){
+
+       
+       if (this.state.errorMessage && !this.state.lat){
+           return <div>Error: {this.state.errorMessage}</div>;
+       }
+       
+       if(!this.state.errorMessage && this.state.lat){
+       return <SeasonDisplay  lat= {this.state.lat} />   
+       }
+       
+       return <div>Loading! </div>
+         
+    };
+
+}
+
+ReactDOM.render(<App />, document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
